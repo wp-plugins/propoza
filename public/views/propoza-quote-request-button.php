279d563a54e9bd514d3a89b58a12f95dc6dbadf0
info.propoza <info.propoza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 13:13:06 +0000

    Clean trunk
    git-svn-id: https://plugins.svn.wordpress.org/propoza/trunk@1132029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/views/propoza-quote-request-button.php
+++
@@ -1,4 +0,0 @@
-<?php if ( ! Propoza_Quote_Request::get_instance()->has_propoza_coupon() ): ?>
-	<a class="button alt quote-button " name="request"
-	   onclick="request_quote();"><?php echo __( 'Request quote', 'propoza' ); ?></a>
-<?php endif; ?>
--- public/views/propoza-quote-request-dialog.php
+++
@@ -1,3 +0,0 @@
-<div id="dialog" style="display: none">
-
-</div>
